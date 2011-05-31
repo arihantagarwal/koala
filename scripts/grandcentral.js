a8ca9b1605d4c3de342d4ff567a157d6c162305c
Abhinav Sharma <me@abhinavsharma.me>
2011-05-31 14:42:05 -0700

    add grandcentral

---
+++ scripts/grandcentral.js
@@ -0,0 +1,26 @@
+function  GrandCentral() {
+  let me = this;
+  me.utils = new KoalaUtils();
+};
+
+GrandCentral.prototype.findHubFromPlace = function(placeId) {
+  let me = this;
+  // TODO: write this function
+  Cu.reportError(placeId);
+  //let revHost = me.utils.getData(["rev_host"],{"id":placeId},"moz_places")[0]["rev_host"];
+  
+  /*
+  let stm = Svc.History.DBConnection.createAsyncStatement(
+    "SELECT id FROM moz_places where rev_host = :revHost ORDER BY visit_count DESC LIMIT 1");
+  stm.params.revHost = revHost;
+  return Utils.queryAsync(stm, ["id"])[0]["id"];
+  */
+  return 1376;
+};
+
+GrandCentral.prototype.isHub = function(placeId) {
+  // TODO: make efficient, cant make so many queries
+  let me = this;
+  let hub = me.findHubFromPlace(placeId);
+  return (hub == placeId);
+}
