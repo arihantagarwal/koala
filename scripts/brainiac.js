109d662013f00f4d27899720f622ed2ddb197e01
Abhinav Sharma <me@abhinavsharma.me>
2011-06-01 12:40:58 -0700

    adding brain and brainiac

---
+++ scripts/brainiac.js
@@ -0,0 +1,8 @@
+/*
+ * Brainiac is a classifier that tries to detect if a site
+ * is interesting based on parameters. It uses bookmarks as
+ * training data.
+ *
+ * This is initially based on neural networks from the brain
+ * library.
+ */
